d034388af97bb70d6c7228575182bfa58211b5b2
彭和清 <pengheqing@fhic.com>
2018-11-15 20:07:06 +0800

    fix django2.0

--- railguns/cloudfile/widgets.py
+++ railguns/cloudfile/widgets.py
@@ -1,7 +1,7 @@
 import os
 
 from django.conf import settings
-from django.core.urlresolvers import reverse
+from django.urls import reverse
 from django.forms import widgets
 from django.utils.safestring import mark_safe
 
--- railguns/urls.py
+++ railguns/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.decorators.cache import cache_page
-from django.views.generic.base import RedirectView, TemplateView
+from django.views.generic.base import TemplateView
 from django.views.i18n import javascript_catalog
 from rest_framework_jwt.views import obtain_jwt_token, refresh_jwt_token, verify_jwt_token
 
