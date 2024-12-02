5c3908df7553a0edc3266e6136cd96f83b4db5f7
larryhq <346985049@qq.com>
2024-12-02 10:45:59 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     version='0.1.9',
     install_requires=[
         'Django',
-        'djangorestframework<3.10',
+        'djangorestframework',
         'djangorestframework-jwt',
         'djangorestframework-xml',
         'django-rest-swagger',
